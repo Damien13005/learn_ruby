2bb3ade09e782e10b92334501901c7dc0319967a
Damien Palierne <damien.palierne@yahoo.com>
2018-04-09 20:45:51 +0200

    Ruby Methodes Damien & Samir

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,18 @@
-#write your code here
+def ftoc(f)
+    c = (f/1.8) - 17.777
+    return c.to_i
+end
+
+def ctof(f)
+    t = (f*1.8) + 32
+    return t
+end
+
+
+
+
+
+
+
+
+
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,40 @@
-#write your code here
+
+
+def echo(string)
+	string
+end
+
+def shout(hello)
+	hello.upcase
+end
+
+def repeat(word, number = 2)
+        ([word] * number).join(" ")
+end
+
+def start_of_word(string, n=1)
+	string[0..n-1] #print la premiere lettre de ta variable#
+end
+
+
+def first_word(string)
+	string.split[0]
+end
+
+def titleize(string)
+	littleWords = ["and", "the", "over"]
+	s = string.split.each {|mot| mot.capitalize! unless littleWords.include? (mot.downcase) }
+	n = s.length 
+	if n == 1
+		s[0].capitalize
+	else
+		s[0].capitalize + " " + s[1...n].join(" ")
+	end
+end
+
+
+
+
+
+
+
